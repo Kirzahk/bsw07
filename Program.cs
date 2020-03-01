a766993adca76f83bdacd3fa10c18ac0a2748d1c
Kirzahk <61593550+Kirzahk@users.noreply.github.com>
2020-03-01 20:25:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Ejemplo07___console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Otra forma de ahorrar pulsaciones");
+        }
+    }
+}
